75db707b99f4ce349b8bd84f33aa298b10a9805a
mbearskin <mw4k3@mst.edu>
2015-04-07 17:29:57 -0500

    created traits object migration and seed

---
+++ app/database/migrations/2015_04_07_214920_create_traits_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTraitsTable extends Migration {
+
+
+    public function up()
+    {
+        Schema::create('traits', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('sheet_id');
+            $table->string('title', 150);
+            $table->text('desc');
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExisits('traits');
+    }
+
+}
