6a45057b0109f617e86e87f73bacfc228e54c990
mbearskin <mw4k3@mst.edu>
2015-04-13 20:07:45 -0500

    Created CharSpell object, migration and seed. Also added my local host name.

---
+++ app/DungeonCrawler/Objects/CharSpell.php
@@ -0,0 +1,17 @@
+<?php namespace DungeonCrawler\Objects;
+class CharSpell extends \Eloquent {
+
+    protected $table = 'char_spells';
+
+    protected $guarded = array('id');
+
+    public function CharacterSheet()
+    {
+        return $this->belongsTo('DungeonCrawler\Objects\CharacterSheet', 'sheet_id', 'id');
+    }
+
+    public function Spell()
+    {
+         //return $this->hasOne('DungeonCrawler\Objects\Spell');
+    }
+}
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder {
         $this->call('TraitsTableSeeder');
         $this->call('AttackSpellTableSeeder');
         $this->call('CharacterSheetTableSeeder');
+        $this->call('CharSpellTableSeeder');
     }
 
 }
