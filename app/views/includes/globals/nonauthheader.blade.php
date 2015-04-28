e2e7df97ff28510a6447fcb22d4e17bc5394c141
Chris Garlock <garlockchristopher@gmail.com>
2015-04-27 21:33:11 -0500

    Added header to non-auth pages only and created home page.

--- app/views/includes/globals/nonauthheader.blade.php
+++ app/views/includes/globals/nonauthheader.blade.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Chris
- * Date: 4/27/2015
- * Time: 9:27 PM
- */
+<h2 class="ui segment header block">
+    Campaign Crawler
+    <div class="ui right floated header">
+        <div class="ui button" id="homeButton"><a href="/">Home</a></div>
+        <div class="ui button" id="loginButton"><a href="/login">Login</a></div>
+        <div class="ui button" id="registerButton"><a href="/register">Register</a></div>
+    </div>
+</h2>
